180d9ac9e673ffc228763972c1eb8fb92ae778ef
DoHyun Choi <dohyen94@naver.com>
2016-12-04 12:22:27 +0900

    Test1

---
+++ materiallogindemo-master/app/src/main/java/com/sourcey/materiallogindemo/SignupActivity.java
@@ -0,0 +1,160 @@
+package com.sourcey.materiallogindemo;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.Bind;
+
+public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "SignupActivity";
+
+    @Bind(R.id.input_name) EditText _nameText;
+    @Bind(R.id.input_address) EditText _addressText;
+    @Bind(R.id.input_email) EditText _emailText;
+    @Bind(R.id.input_mobile) EditText _mobileText;
+    @Bind(R.id.input_password) EditText _passwordText;
+    @Bind(R.id.input_reEnterPassword) EditText _reEnterPasswordText;
+    @Bind(R.id.btn_signup) Button _signupButton;
+    @Bind(R.id.link_login) TextView _loginLink;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        _signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+    }
+
+    public void signup() {
+        Log.d(TAG, "가입하기");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        _signupButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("계정을 생성중입니다...");
+        progressDialog.show();
+
+        String name = _nameText.getText().toString();
+        String address = _addressText.getText().toString();
+        String email = _emailText.getText().toString();
+        String mobile = _mobileText.getText().toString();
+        String password = _passwordText.getText().toString();
+        String reEnterPassword = _reEnterPasswordText.getText().toString();
+
+        // TODO: Implement your own signup logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    public void onSignupSuccess() {
+        _signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "로그인 실패", Toast.LENGTH_LONG).show();
+
+        _signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = _nameText.getText().toString();
+        String address = _addressText.getText().toString();
+        String email = _emailText.getText().toString();
+        String mobile = _mobileText.getText().toString();
+        String password = _passwordText.getText().toString();
+        String reEnterPassword = _reEnterPasswordText.getText().toString();
+
+        if (name.isEmpty() || name.length() < 3) {
+            _nameText.setError("최소 3개의 문자를 입력해주세요");
+            valid = false;
+        } else {
+            _nameText.setError(null);
+        }
+
+        if (address.isEmpty()) {
+            _addressText.setError("올바른 주소를 입력해주세요");
+            valid = false;
+        } else {
+            _addressText.setError(null);
+        }
+
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("올바른 Email 주소로 입력해주세요");
+            // _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (mobile.isEmpty() || mobile.length()!=10) {
+            _mobileText.setError("올바른 번호로 입력해주세요");
+            valid = false;
+        } else {
+            _mobileText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("4개이상 10개 이하의 영문 숫자조합으로 입력해주세요");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
+            _reEnterPasswordText.setError("비밀번호가 일치하지않습니다.");
+            valid = false;
+        } else {
+            _reEnterPasswordText.setError(null);
+        }
+
+        return valid;
+    }
+}
